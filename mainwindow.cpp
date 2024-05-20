20dce61ec5041f5d97902cc6df5a2c681b584f21
KANKAN25 <hyakana5@gmail.com>
2024-05-20 16:58:39 +0800

    copy qt project

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,27 @@
+// mainwindow.cpp
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->verticalScrollBar, &QScrollBar::valueChanged, this, &MainWindow::on_verticalScrollBar_valueChanged);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_verticalScrollBar_valueChanged(int value)
+{
+    ui->filterarea->move(0, -value);
+}
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_verticalScrollBar_valueChanged(int value);
+
 private:
     Ui::MainWindow *ui;
 };
