f8ced2917a3afd6d583c95c7dd8dbb902bfb49c1
JohSANCoding <129604435+JohSANCoding@users.noreply.github.com>
2024-05-21 18:55:02 +0800

    Some other Things

--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 #include <QMainWindow>
 
@@ -19,8 +22,10 @@ public:
 
 private slots:
     void on_verticalScrollBar_valueChanged(int value);
+//    void on_loadJsonButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    void loadJsonData();
 };
 #endif // MAINWINDOW_H
