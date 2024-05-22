9bc52f72dc70d6cae4689fd31cb10986d052f7ff
JohSANCoding <129604435+JohSANCoding@users.noreply.github.com>
2024-05-22 09:19:00 +0800

    Build Removal
    Build doesn't need to be passed along to git

--- main.cpp
+++ main.cpp
@@ -2,14 +2,42 @@
 
 #include <QApplication>
 #include "json.hpp"
+#include <fstream>
+#include <iostream>
+
 
 using json = nlohmann::json;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    ifstream file("people.json");
+    json j;
+    file >> j;
+    file.close();
+
+    // Create a new person object
+    json newPerson;
+    newPerson["name"] = "Alice Brown";
+    newPerson["age"] = 28;
+    newPerson["occupation"] = "Teacher";
+    json address;
+    address["street"] = "901 Maple St";
+    address["city"] = "Newtown";
+    address["state"] = "FL";
+    address["zip"] = "12345";
+    newPerson["address"] = address;
+
+    // Append the new person object to the existing JSON array
+    j.push_back(newPerson);
+
+    // Write the updated JSON data to the file
+    ofstream fileOut("people.json");
+    fileOut << j.dump(4); // 4 is the indentation level
+    fileOut.close();
 
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 #include "json.hpp"
 
+
 using json = nlohmann::json;
 
 #include <QMainWindow>
