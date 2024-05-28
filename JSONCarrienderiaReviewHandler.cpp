52d59d2bd9a687c3de67394795890f1ba7733817
JohSANCoding <129604435+JohSANCoding@users.noreply.github.com>
2024-05-28 19:52:16 +0800

    This is TEMP

--- JSONCarrienderiaReviewHandler.cpp
+++ JSONCarrienderiaReviewHandler.cpp
@@ -5,55 +5,63 @@
 // To be polished and made
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 JSONCarrienderiaReviewHandler::JSONCarrienderiaReviewHandler()
 {
     // initializeBuffers();
 }
+=======
+// JSONCarrienderiaReviewHandler::JSONCarrienderiaReviewHandler()
+// {
+//     // initializeBuffers();
+// }
+>>>>>>> Stashed changes
 
 
-void JSONCarrienderiaReviewHandler::addReview(const std::string& category, const ordered_json& item)
-{
-    // if (buffers.find(category) != buffers.end())
-    // {
-    //     buffers[category].push_back(item);
-    //     counters[category]++;
-    // }
-}
+// void JSONCarrienderiaReviewHandler::addReview(const std::string& category, const ordered_json& item)
+// {
+//     // if (buffers.find(category) != buffers.end())
+//     // {
+//     //     buffers[category].push_back(item);
+//     //     counters[category]++;
+//     // }
+// }
 
-void JSONCarrienderiaReviewHandler::editReview(const std::string& category, size_t index, const ordered_json& item)
-{
-    // if (buffers.find(category) != buffers.end() && index < buffers[category].size())
-    // {
-    //     buffers[category][index] = item;
-    // }
-}
+// void JSONCarrienderiaReviewHandler::editReview(const std::string& category, size_t index, const ordered_json& item)
+// {
+//     // if (buffers.find(category) != buffers.end() && index < buffers[category].size())
+//     // {
+//     //     buffers[category][index] = item;
+//     // }
+// }
 
-void JSONCarrienderiaReviewHandler::deleteReview(const std::string& category, size_t index)
-{
-    // if (buffers.find(category) != buffers.end() && index < buffers[category].size())
-    // {
-    //     buffers[category].erase(buffers[category].begin() + index);
-    //     counters[category]--;
-    // }
-}
+// void JSONCarrienderiaReviewHandler::deleteReview(const std::string& category, size_t index)
+// {
+//     // if (buffers.find(category) != buffers.end() && index < buffers[category].size())
+//     // {
+//     //     buffers[category].erase(buffers[category].begin() + index);
+//     //     counters[category]--;
+//     // }
+// }
 
-void JSONCarrienderiaReviewHandler::save(const std::string& filename)
-{
-    // for (const auto& pair : buffers)
-    // {
-    //     carrienderia["Menu Offerings"][pair.first] = pair.second;
-    // }
+// void JSONCarrienderiaReviewHandler::save(const std::string& filename)
+// {
+//     // for (const auto& pair : buffers)
+//     // {
+//     //     carrienderia["Menu Offerings"][pair.first] = pair.second;
+//     // }
 
-    // std::ofstream file(filename);
-    // if (!file.is_open())
-    // {
-    //     std::cerr << "Unable to open file " << filename << std::endl;
-    //     return;
-    // }
-    // file << carrienderia.dump(4);
-    // file.close();
-}
+//     // std::ofstream file(filename);
+//     // if (!file.is_open())
+//     // {
+//     //     std::cerr << "Unable to open file " << filename << std::endl;
+//     //     return;
+//     // }
+//     // file << carrienderia.dump(4);
+//     // file.close();
+// }
 
+<<<<<<< Updated upstream
 const ordered_json& JSONCarrienderiaReviewHandler::getItems(const std::string& category) const {
     // return buffers.at(category);
 }
@@ -107,6 +115,11 @@ const ordered_json& JSONCarrienderiaReviewHandler::getItems(const std::string& c
 //     // file.close();
 // }
 
+// const ordered_json& JSONCarrienderiaReviewHandler::getItems(const std::string& category) const {
+//     // return buffers.at(category);
+// }
+>>>>>>> Stashed changes
+=======
 // const ordered_json& JSONCarrienderiaReviewHandler::getItems(const std::string& category) const {
 //     // return buffers.at(category);
 // }
--- JSONCarrienderiaReviewHandler.h
+++ JSONCarrienderiaReviewHandler.h
@@ -2,29 +2,36 @@
 #define JSONCARRIENDERIAREVIEWHANDLER_H
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 #include "json.hpp"
 #include <vector>
 #include <string>
+=======
+// #include "json.hpp"
+// #include <vector>
+// #include <string>
+>>>>>>> Stashed changes
 
-// Work in progress header file for carrienderia review handling
+// // Work in progress header file for carrienderia review handling
 
-using ordered_json = nlohmann::ordered_json;
+// using ordered_json = nlohmann::ordered_json;
 
-class JSONCarrienderiaReviewHandler
-{
-public:
-    JSONCarrienderiaReviewHandler();
+// class JSONCarrienderiaReviewHandler
+// {
+// public:
+//     JSONCarrienderiaReviewHandler();
 
-    // void addReview(const std::string& category, const ordered_json& item);
-    // void editReview(const std::string& category, size_t index, const ordered_json& item);
-    // void deleteReview(const std::string& category, size_t index);
-    // void save(const std::string& filename);
+//     // void addReview(const std::string& category, const ordered_json& item);
+//     // void editReview(const std::string& category, size_t index, const ordered_json& item);
+//     // void deleteReview(const std::string& category, size_t index);
+//     // void save(const std::string& filename);
 
-    // const ordered_json& getItems(const std::string& category) const;
+//     // const ordered_json& getItems(const std::string& category) const;
 
-private:
-    // ordered_json carrienderia;
+// private:
+//     // ordered_json carrienderia;
 
+<<<<<<< Updated upstream
     // void initializeBuffers();
 };
 =======
@@ -51,6 +58,10 @@ private:
 // private:
 //     // ordered_json carrienderia;
 
+//     // void initializeBuffers();
+// };
+>>>>>>> Stashed changes
+=======
 //     // void initializeBuffers();
 // };
 >>>>>>> Stashed changes
